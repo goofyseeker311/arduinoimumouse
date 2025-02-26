aa53da35b50664d7d801f3c8700c3c8db2f20cf3
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-26 10:47:41 +0200

    Update arduinoimumouse.ino
    Removed implementation, added basic mouse, imu and generic arduino test code.

--- arduinoimumouse.ino
+++ arduinoimumouse.ino
@@ -1,30 +1,38 @@
 #include <Mouse.h>
 #include <Arduino_LSM6DS3.h>
-float ax=0.0f, ay=0.0f, az=1.01f, tol=0.01f, ltol=0.1f, dec=0.99f, mlt=2000000;
-unsigned long pstime=0, lstime=0, ldtime=100000;
+unsigned long pstime=0, cstime=0;
+bool ledon = false;
 void setup() {
-  pstime=micros(); lstime=pstime;
+  Serial.begin(9600);
+  delay(3000);
+  Serial.println("led: begin");
+  pstime = micros(); cstime = pstime;
   pinMode(LED_BUILTIN, OUTPUT);
-  if (!IMU.begin()) {
-      while (1);
-  }
+  if (!IMU.begin()) {while (1);}
   Mouse.begin();
 }
 void loop() {
-  float x, y, z; float fps; float dx, dy, dz, dt, dt2; int mx, my; bool mousemove = false; bool mouselift = false; unsigned long ntime = micros();
+  float x, y, z, dt;
+  bool mousemove = false, mouselift = false;
+  pstime = cstime; cstime = micros();
+  unsigned long ds = cstime - pstime;
+  if (!ledon) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    ledon = true;
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);
+    ledon = false;
+  }
+  delay(100);
   if (IMU.accelerationAvailable()) {
-      IMU.readAcceleration(y, x, z); fps = IMU.accelerationSampleRate(); dt=1.0f/fps; dt2=dt*dt;
-      ax=dec*ax+(1.0f-dec)*x; ay=dec*ay+(1.0f-dec)*y; az=dec*az+(1.0f-dec)*z; dx=x-ax; dy=y-ay; dz=z-az;
-      if (abs(dx)>tol) { mx=mlt*0.5*dx*dt2; mousemove = true; }
-      if (abs(dy)>tol) { my=mlt*0.5*dy*dt2; mousemove = true; }
-      if (abs(dz)>ltol) { mouselift = true; lstime = micros(); }
-      if ((mouselift) || (ntime<(lstime+ldtime))) {
-        digitalWrite(LED_BUILTIN, HIGH);  // turn the LED on (HIGH is the voltage level)
-      } else {
-        digitalWrite(LED_BUILTIN, LOW);   // turn the LED off by making the voltage LOW
-        if (mousemove) {
-          Mouse.move(mx, my, 0);
-        }
-      }
+      IMU.readAcceleration(y, x, z);
+      dt = 1.0f/IMU.accelerationSampleRate();
+      int xi = (int)(x*1000000);
+      int yi = (int)(y*1000000);
+      int zi = (int)(z*1000000);
+      int dti = (int)(dt*1000000);
+      char buffer[50];
+      sprintf(buffer, "ds: %luus, x: %d, y: %d, z: %d, dt: %dus",ds,xi,yi,zi,dti);
+      Serial.println(buffer);
   }
 }
