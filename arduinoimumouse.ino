a95f3bf4673658c956f7c2f0eb5f732b43e11a84
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-26 13:33:38 +0200

    Update arduinoimumouse.ino
    Implemented mean() and stdev() functions to calculate mean noise variance of input accelerometer axes to determine if mouse is moving or being lifted.

--- arduinoimumouse.ino
+++ arduinoimumouse.ino
@@ -1,11 +1,16 @@
 //#include <Mouse.h>
 #include <Arduino_LSM6DS3.h>
 #define grav 9.82f
+#define bufs 50
+#define movelim 0.1f
+#define liftlim 0.1f
 unsigned long pstime=0, cstime=0;
 float x=0, y=0, z=0, f=IMU.accelerationSampleRate(), dt=1.0f/f;
 float dx=0, dy=0, dz=0;
-bool ledon = false;
+float dxarray[bufs], dyarray[bufs], dzarray[bufs];
+int arrayind = 0;
 void setup() {
+  for (int i=0;i<bufs;i++) {dxarray[i]=0;dyarray[i]=0;dzarray[i]=0;}
   Serial.begin(9600);
   while(!Serial);
   Serial.println("led: begin");
@@ -18,25 +23,59 @@ void loop() {
   bool mousemove = false, mouselift = false;
   pstime = cstime; cstime = micros();
   unsigned long ds = cstime - pstime;
-  if (!ledon) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    ledon = true;
-  } else {
-    digitalWrite(LED_BUILTIN, LOW);
-    ledon = false;
-  }
-  delay(100);
   if (IMU.accelerationAvailable()) {
       IMU.readAcceleration(y, x, z);
       dx = grav*x;
       dy = grav*y;
       dz = grav*z;
-      int dxi = (int)(dx*1000000);
-      int dyi = (int)(dy*1000000);
-      int dzi = (int)(dz*1000000);
-      int dti = (int)(dt*1000000);
+      dxarray[arrayind] = dx;
+      dyarray[arrayind] = dy;
+      dzarray[arrayind] = dz;
+      if (++arrayind>=bufs) {arrayind=0;}
+      float xm = mean(dxarray, bufs);
+      float ym = mean(dyarray, bufs);
+      float zm = mean(dzarray, bufs);
+      float xs = stdev(dxarray, bufs);
+      float ys = stdev(dyarray, bufs);
+      float zs = stdev(dzarray, bufs);
+      if ((xs>movelim)||(ys>movelim)) {
+        mousemove = true;
+      }
+      if (zs>movelim) {
+        mouselift = true;
+      }
+      if (mouselift) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("mouselift");
+      } else if (mousemove) {
+        digitalWrite(LED_BUILTIN, HIGH);
+        Serial.println("mousemove");
+      } else {
+        digitalWrite(LED_BUILTIN, LOW);
+      }
       char buffer[100];
-      sprintf(buffer, "ds: %luus, dx: %dum/s^2, dy: %dum/s^2, dz: %dum/s^2, dt: %dus",ds,dxi,dyi,dzi,dti);
+      sprintf(buffer, "ds: %luus, dxm: %d(%d)um/s^2, dym: %d(%d)um/s^2, dzm: %d(%d)um/s^2, dt: %dus",ds,(int)(xm*1000000),(int)(xs*1000000),(int)(ym*1000000),(int)(ys*1000000),(int)(zm*1000000),(int)(zs*1000000),(int)(dt*1000000));
       Serial.println(buffer);
   }
+  delay(5);
+}
+float mean(float* data, int size) {
+  float meanval = 0;
+  for (int i=0;i<size;i++) {
+    meanval += data[i];
+  }
+  meanval /= (float)size;
+  return meanval;
+}
+float stdev(float* data, int size) {
+  float stdval = 0;
+  float meanval = mean(data, size);
+  float varval = 0;
+  for (int i=0;i<size;i++) {
+    float difval = data[i]-meanval;
+    varval += pow(difval,2);
+  }
+  varval /= ((float)size);
+  stdval = sqrt(varval);
+  return stdval;
 }
