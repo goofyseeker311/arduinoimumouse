6014d962d29b23020e9aede1f46ba0d5627d09f5
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-26 11:51:50 +0200

    Update arduinoimumouse.ino
    Added gravity to acceleration conversion.
    Changed initial delay to Serial status wait while loop.

--- arduinoimumouse.ino
+++ arduinoimumouse.ino
@@ -1,18 +1,20 @@
-#include <Mouse.h>
+//#include <Mouse.h>
 #include <Arduino_LSM6DS3.h>
+#define grav 9.82f
 unsigned long pstime=0, cstime=0;
+float x=0, y=0, z=0, f=IMU.accelerationSampleRate(), dt=1.0f/f;
+float dx=0, dy=0, dz=0;
 bool ledon = false;
 void setup() {
   Serial.begin(9600);
-  delay(3000);
+  while(!Serial);
   Serial.println("led: begin");
   pstime = micros(); cstime = pstime;
   pinMode(LED_BUILTIN, OUTPUT);
-  if (!IMU.begin()) {while (1);}
-  Mouse.begin();
+  if (!IMU.begin()) {Serial.println("IMU.begin() failed."); while (1);}
+  //Mouse.begin();
 }
 void loop() {
-  float x, y, z, dt;
   bool mousemove = false, mouselift = false;
   pstime = cstime; cstime = micros();
   unsigned long ds = cstime - pstime;
@@ -26,13 +28,15 @@ void loop() {
   delay(100);
   if (IMU.accelerationAvailable()) {
       IMU.readAcceleration(y, x, z);
-      dt = 1.0f/IMU.accelerationSampleRate();
-      int xi = (int)(x*1000000);
-      int yi = (int)(y*1000000);
-      int zi = (int)(z*1000000);
+      dx = grav*x;
+      dy = grav*y;
+      dz = grav*z;
+      int dxi = (int)(dx*1000000);
+      int dyi = (int)(dy*1000000);
+      int dzi = (int)(dz*1000000);
       int dti = (int)(dt*1000000);
-      char buffer[50];
-      sprintf(buffer, "ds: %luus, x: %d, y: %d, z: %d, dt: %dus",ds,xi,yi,zi,dti);
+      char buffer[100];
+      sprintf(buffer, "ds: %luus, dx: %dum/s^2, dy: %dum/s^2, dz: %dum/s^2, dt: %dus",ds,dxi,dyi,dzi,dti);
       Serial.println(buffer);
   }
 }
