09303b827ef7508ba6ea605fe26f8811832d6a87
goofyseeker311 <goofyseeker311@gmail.com>
2025-02-27 08:47:50 +0200

    Update arduinoimumouse.ino
    Removed mouse control, restored serial debug for mouse position simulation.

--- arduinoimumouse.ino
+++ arduinoimumouse.ino
@@ -2,10 +2,10 @@
 #include <Arduino_LSM6DS3.h>
 #define grav 9.82f
 #define bufs 4
-#define movelim 0.1f
-#define liftlim 0.1f
+#define movelim 0.05f
+#define liftlim 0.05f
 #define movemult 100.0f
-//char buffer[200];
+char buffer[200];
 unsigned long pstime=0, cstime=0;
 float mousex=0, mousey=0;
 float x=0, y=0, z=0, f=IMU.accelerationSampleRate(), dt=1.0f/f;
@@ -15,16 +15,17 @@ float dxarray[bufs], dyarray[bufs], dzarray[bufs];
 int arrayind = 0;
 void setup() {
   for (int i=0;i<bufs;i++) {dxarray[i]=0;dyarray[i]=0;dzarray[i]=0;}
-  //Serial.begin(9600);
-  //while(!Serial);
-  //Serial.println("led: begin");
+  Serial.begin(9600);
+  while(!Serial);
+  Serial.println("serial: begin");
   pstime = micros(); cstime = pstime;
   pinMode(LED_BUILTIN, OUTPUT);
   if (!IMU.begin()) {
-    //Serial.println("IMU.begin() failed.");
+    Serial.println("IMU.begin() failed.");
     while (1);
   }
   Mouse.begin();
+  Serial.println("mouse: begin");
 }
 void loop() {
   bool mousemove = false, mouselift = false;
@@ -63,9 +64,11 @@ void loop() {
       }
       float mousedx = sx*ds*movemult;
       float mousedy = sy*ds*movemult;
-      Mouse.move((char)mousedx,(char)mousedy,0);
-      //sprintf(buffer,"%lu: mousemove mousedx: %d, mousedy: %d",cstime,(int)(mousedx*1000000.0f),(int)(mousedy*1000000.0f));
-      //Serial.println(buffer);
+      mousex += mousedx;
+      mousey -= mousedy;
+      //Mouse.move(mousedx,mousedy,0);
+      sprintf(buffer,"%lu: mousemove mousedx: %d, mousedy: %d, mousex: %d, mousey: %d",cstime,(int)(mousedx*1000000.0f),(int)(mousedy*1000000.0f),(int)(mousex*1000000.0f),(int)(mousey*1000000.0f));
+      Serial.println(buffer);
       //sprintf(buffer, "ds: %dus, dx: %d(%d)um/s^2, dy: %d(%d)um/s^2, dz: %d(%d)um/s^2, dt: %dus",(int)(ds*1000000.0f),(int)(dx*1000000.0f),(int)(xs*1000000.0f),(int)(dy*1000000.0f),(int)(ys*1000000.0f),(int)(dz*1000000.0f),(int)(zs*1000000.0f),(int)(dt*1000000.0f));
       //Serial.println(buffer);
   }
